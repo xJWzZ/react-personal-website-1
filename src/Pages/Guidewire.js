ee6795dd38d2bcee07d415c3d3779696b602e351
Jack Wall <jackw1997@gmail.com>
2023-08-21 12:47:51 +0100

    Add Guidewire page

---
+++ src/Pages/Guidewire.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import WorkExperienceSection from '../Components/WorkExperienceSection'
+
+function Guidewire() {
+  return (
+    <>
+        <WorkExperienceSection></WorkExperienceSection>
+    </>
+  )
+}
+
+export default Guidewire
--- src/Pages/WorkExperience.js
+++ src/Pages/WorkExperience.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import '../App.css'
-import WorkExperienceSection from '../Components/WorkExperienceSection'
 import Cards from '../Components/Cards'
 
 export default function WorkExperience() {
     return (
         <>
-            {/* <WorkExperienceSection></WorkExperienceSection> */}
             <Cards></Cards>
         </>
       )
