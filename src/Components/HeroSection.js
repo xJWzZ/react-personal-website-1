d79bd9b91eb2e5a1644a0b929b148cdfd42767d7
Jack Wall <jackw1997@gmail.com>
2023-08-24 11:01:26 +0100

    Change background file extension. This is an attempt to fix issue where background isn't displaying in AWS Amplify

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button } from './Button'
 import './HeroSection.css'
 import '../App.css'
-import background from "../images/background.JPG"
+import background from "../images/HomePageBackground.jpg"
 import { Link } from 'react-router-dom'
 
 function HeroSection() {
