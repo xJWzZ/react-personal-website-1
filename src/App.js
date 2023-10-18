7eaa7478a1d1395acc87c073d0de57cb69153bb8
Jack Wall <116397063@umail.ucc.ie>
2023-10-18 14:42:22 +1300

    Fix mobile image positioning problem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import Navbar from './Components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
